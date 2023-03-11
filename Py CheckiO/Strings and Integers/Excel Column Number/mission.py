82015d782f0999104ef0ef9a50878ae049911084
Emil Enchev <emoen4ev@gmail.com>
2023-03-11 15:03:08 +0200

    Added the file structure of the 'Py CheckiO' course.

---
+++ Py CheckiO/Strings and Integers/Excel Column Number/mission.py
@@ -0,0 +1,15 @@
+def column_number(name: str) -> int:
+    # your code here
+    return 0
+
+
+print("Example:")
+print(column_number("AA"))
+
+# These "asserts" are used for self-checking
+assert column_number("A") == 1
+assert column_number("Z") == 26
+assert column_number("AB") == 28
+assert column_number("ZY") == 701
+
+print("The first mission is done! Click 'Check' to earn cool rewards!")
